0137715dfd6c540b763bf1d0b3260bacbcca3e68
Jorge Lucas <jorgelucas77@gmail.com>
2021-02-18 16:45:52 +0200

    Adiciona tag ao conteudo
    Tarefa:
    https://trello.com/c/irSmtn7B/10-permitir-adicionar-tag-nos-conte%C3%BAdos

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,33 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
+/* import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import $ from "jquery"
+import "select2" */
 
-Rails.start()
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
+
+/* Rails.start()
 Turbolinks.start()
-ActiveStorage.start()
+ActiveStorage.start() */
+
+
+import $ from 'jquery'
+import 'select2'
+
+$(document).on('turbolinks:load', function() {
+    $(".select2").select2({
+      tags: true,
+      tokenSeparators: [',', ' ']
+    })
+  });
+  
+  
+
